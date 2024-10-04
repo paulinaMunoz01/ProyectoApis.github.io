63fc9cc73625f7d823385283328eba45524ae9c2
Paulina.Munoz <160296357+paulinaMunoz01@users.noreply.github.com>
2024-10-04 09:07:45 -0600

    staticmap.js

--- staticmap.js
+++ staticmap.js
@@ -1,9 +1,27 @@
+// Initialize and add the map
 let map;
+
 async function initMap() {
+  // The location of Uluru
+  const position = { lat: 21.814326449341447, lng: -102.76976863232528 };
+  // Request needed libraries.
+  //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
   map = new Map(document.getElementById("map"), {
-    center: { lat: 21.81431648876566, lng: -102.76975790348973 },
-    zoom: 15,
+    zoom: 14,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Uluru",
   });
 }
+
 initMap();
