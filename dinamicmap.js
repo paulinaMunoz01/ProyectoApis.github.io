c672e13e96f523cf6837802b79f610742bcf23ac
Paulina.Munoz <160296357+paulinaMunoz01@users.noreply.github.com>
2024-10-08 18:04:06 -0600

    dinamicmap.js

--- dinamicmap.js
+++ dinamicmap.js
@@ -1,4 +1,4 @@
-let map, directionsService, directionsRenderder;
+let map, directionsService, directionsRenderer;
 
 function initMap() {
     const myLatLng = { lat: 21.81428552567272, lng: -102.76898188728283 };
@@ -9,11 +9,11 @@ function initMap() {
 
     //Servicio y renderizador de direcciones (ruta)
     directionsService = new google.maps.DirectionsService();
-    directionsRenderder = new google.maps.DirectionsRenderder();
-    directionsRenderder.setMap(map);
+    directionsRenderer = new google.maps.DirectionsRenderer();
+    directionsRenderer.setMap(map);
 
     const image =
-        "./universidad.png";  //CREAMOS LA CONSTANTE CON LA RUTA DE LA IMAGEN
+        "./zoo.png";  //CREAMOS LA CONSTANTE CON LA RUTA DE LA IMAGEN
     new google.maps.Marker({
         position: myLatLng,
         map,
@@ -80,53 +80,60 @@ function initMap() {
                     bounds.extend(place.geometry.location);
                 }
 
-                /* Calcular y mostrar la ruta */
+                //Calcular y mostrar la ruta
                 calculateAndDisplayRoute(place.geometry.location);
             });
             map.fitBounds(bounds);
+
         }
     );
+
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
-            (position) => {
-                const pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude,
-                };
-                map.setCenter(pos);
-            },
-            () => {
-                console.log("Error obteniendo la ubicacion actual");
-            }
+          (position) => {
+            const pos = {
+              lat: position.coords.latitude,
+              lat: position.coords.longitude,
+            };
+            map.setCenter(pos);
+          },
+          () => {
+            console.log("Error obtenido la ubicacion actual.");
+          }
         );
     }
+
+
+
+
 }
 
 // Función para calcular y mostrar la ruta desde la ubicación actual hasta el lugar seleccionado
 function calculateAndDisplayRoute(destination) {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-            const origin = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude,
-            };
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+          const origin = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+          };
 
-            directionsService.route(
-                {
-                    origin: origin,
-                    destination: destination,
-                    travelMode: google.maps.TravelMode.DRIVING, // Puedes cambiarlo a WALKING, BICYCLING, etc.
-                },
-                (response, status) => {
-                    if (status === "OK") {
-                        directionsRenderer.setDirections(response);
-                    } else {
-                        console.log("Direcciones fallidas: " + status);
-                    }
-                }
-            );
-        });
-    }
+          directionsService.route(
+              {
+                  origin: origin,
+                  destination: destination,
+                  travelMode: google.maps.TravelMode.DRIVING, // Puedes cambiarlo a WALKING, BICYCLING, etc.
+              },
+              (response, status) => {
+                  if (status === "OK") {
+                      directionsRenderer.setDirections(response);
+                  } else {
+                      console.log("Direcciones fallidas: " + status);
+                  }
+              }
+          );
+      });
+  }
 }
 
 initMap();
+/* 21.81428552567272, -102.76898188728283 */
